86e02d2785b5dd746081d75fd1fc65d078b1735a
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2024-07-25 23:00:16 +0700

    Update devcrave.sh

--- devcrave.sh
+++ devcrave.sh
@@ -1,5 +1,6 @@
 crave clone create --projectID 86 /crave-devspaces/Rising14
 cd Rising14
+git clone https://github.com/hetdahlu/prindanpan vendor/lineage-priv/
 crave run  --no-patch -- "rm -rf .repo/local_manifests; \
 git clone https://github.com/hetdah/local_manifests --depth 1 -b rising .repo/local_manifests; \
 /opt/crave/resync.sh; \
